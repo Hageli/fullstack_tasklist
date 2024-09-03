6985384ba638a97476072af45dded10484a75d63
Hageli <miro.hageli@gmail.com>
2024-09-03 12:27:30 +0300

    Servers done for part NodeJS

---
+++ index.js
@@ -0,0 +1,13 @@
+import getPosts, { getPostsLength}  from './postController.js';
+
+console.log(getPosts());
+
+console.log(getPostsLength());
+
+/*
+ const { generateRandomNumber, celciusToFahrenheit } = require('./utils.js');
+
+console.log(`Random Number: ${generateRandomNumber()}`);
+
+console.log(`Celcius to fahrenheit: ${celciusToFahrenheit(0)}`);
+*/
