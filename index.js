3ec3d3f7060a4c89a7b088d3e83b81b7fc3c431f
Hageli <miro.hageli@gmail.com>
2024-09-03 14:14:17 +0300

    tasklist part MongoDB done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import getPosts, { getPostsLength}  from './postController.js';
+// import getPosts, { getPostsLength}  from './postController.js';
 
-console.log(getPosts());
+// console.log(getPosts());
 
-console.log(getPostsLength());
+// console.log(getPostsLength());
 
 /*
  const { generateRandomNumber, celciusToFahrenheit } = require('./utils.js');
@@ -10,4 +10,14 @@ console.log(getPostsLength());
 console.log(`Random Number: ${generateRandomNumber()}`);
 
 console.log(`Celcius to fahrenheit: ${celciusToFahrenheit(0)}`);
+
+
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri, { useNewUrlParser: true});
+client.connect(async err => {
+    const collection = client.db("blog").collection("posts");
+    console.log(collection);
+    client.close()
+})
 */
